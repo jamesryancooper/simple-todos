6d9715ce54f601285b20b5523272a0bedf5106ea
James Ryan Cooper <jamesryancooper@hotmail.com>
2015-05-19 22:25:04 -0500

    Initial commit

---
+++ simple-todos.js
@@ -0,0 +1,17 @@
+// simple-todos.js
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
